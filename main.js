ec7779b9a382183cb4458847b6c0de12ab6251c2
badal-1 <81519542+badal-1@users.noreply.github.com>
2021-09-19 12:54:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function adduser(){
+var w = document.getElementById("rtyuiopuserid").value;
+localStorage.setItem("usernsilsostfc.c", w);
+var t = document.getElementById("dfi").value;
+if (t == "amover18"){
+window.location="chat.html"; 
+}
+else{
+window.alert("Please type 'amover18' in the text-box");
+}
+}
